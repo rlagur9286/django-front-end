645da4a47996f26b2a3dbd0ef3ddfce29ad2d520
hyuk9286.kim <hyuk9286.kim@navercorp.com>
2018-06-30 11:52:48 +0900

    2018-06-29, H, h -> bootstrap3 restframework 삭제

--- front_end/settings.py
+++ front_end/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    "bootstrap3",
-    "rest_framework",
+    # "bootstrap3",
+    # "rest_framework",
 ]
 
 MIDDLEWARE = [
